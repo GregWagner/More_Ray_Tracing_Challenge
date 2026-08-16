be8893072e906b564ca87fe0a94468be3494c383
GregWagner <theGreg101@gmail.com>
2026-08-16 09:45:08 -0500

    added tuple

---
+++ src/lib/tuple.hpp
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <cmath>
+
+namespace rtc {
+    struct Tuple {
+        static constexpr double EPSILON{1e-9};
+
+        double x_value{};
+        double y_value{};
+        double z_value{};
+        double w_value{}; // 1 for point, 0 for vector
+
+        Tuple() = default;
+
+        Tuple(double x_coord, double y_coord, double z_coord, double w_coord) : x_value{x_coord}, y_value{y_coord},
+            z_value{z_coord}, w_value{w_coord} {
+        }
+
+        static Tuple point(double x_coord, double y_coord, double z_coord) {
+            return Tuple{x_coord, y_coord, z_coord, 1.0};
+        }
+
+        static Tuple vector(double x_coord, double y_coord, double z_coord) {
+            return Tuple{x_coord, y_coord, z_coord, 0.0};
+        }
+
+        [[nodiscard]] bool is_point() const {
+            return std::fabs(w_value - 1.0) < EPSILON;
+        }
+
+        [[nodiscard]] bool is_vector() const {
+            return std::fabs(w_value) < EPSILON;
+        }
+
+        Tuple operator+(const Tuple &other) const {
+            return Tuple{
+                x_value + other.x_value, y_value + other.y_value, z_value + other.z_value, w_value + other.w_value
+            };
+        }
+
+        Tuple operator-(const Tuple &other) const {
+            return Tuple{
+                x_value - other.x_value, y_value - other.y_value, z_value - other.z_value, w_value - other.w_value
+            };
+        }
+
+        Tuple operator*(double scalar) const {
+            return Tuple{x_value * scalar, y_value * scalar, z_value * scalar, w_value * scalar};
+        }
+
+        Tuple operator/(double scalar) const {
+            return Tuple{x_value / scalar, y_value / scalar, z_value / scalar, w_value / scalar};
+        }
+
+        bool operator==(const Tuple &other) const {
+            return std::fabs(x_value - other.x_value) < EPSILON && std::fabs(y_value - other.y_value) < EPSILON &&
+                   std::fabs(z_value - other.z_value) < EPSILON && std::fabs(w_value - other.w_value) < EPSILON;
+        }
+    };
+
+    inline double magnitude(const Tuple &tuple) {
+        return std::sqrt(
+            (tuple.x_value * tuple.x_value) + (tuple.y_value * tuple.y_value) + (tuple.z_value * tuple.z_value) + (
+                tuple.w_value * tuple.w_value));
+    }
+
+    inline Tuple normalize(const Tuple &tuple) {
+        const double mag = magnitude(tuple);
+        return Tuple{tuple.x_value / mag, tuple.y_value / mag, tuple.z_value / mag, tuple.w_value / mag};
+    }
+
+    inline double dot(const Tuple &tuple_a, const Tuple &tuple_b) {
+        return (tuple_a.x_value * tuple_b.x_value) + (tuple_a.y_value * tuple_b.y_value) + (
+                   tuple_a.z_value * tuple_b.z_value) + (
+                   tuple_a.w_value * tuple_b.w_value);
+    }
+
+    inline Tuple cross(const Tuple &tuple_a, const Tuple &tuple_b) {
+        return Tuple::vector((tuple_a.y_value * tuple_b.z_value) - (tuple_a.z_value * tuple_b.y_value),
+                             (tuple_a.z_value * tuple_b.x_value) - (tuple_a.x_value * tuple_b.z_value),
+                             (tuple_a.x_value * tuple_b.y_value) - (tuple_a.y_value * tuple_b.x_value));
+    }
+} // namespace rtc
---
+++ tests/test_tuple.cpp
@@ -0,0 +1,76 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_approx.hpp>
+
+#include "lib/tuple.hpp"
+
+using namespace rtc;
+using Catch::Approx;
+
+SCENARIO("A tuple with w = 1.0 is a point", "[tuple][bdd]") {
+    GIVEN("a <- tuple(4.3, -4.2, 3.1, 1.0)") {
+        Tuple a(4.3, -4.2, 3.1, 1.0);
+
+        THEN("a.x == 4.3") {
+            REQUIRE(a.x_value == Approx(4.3));
+        }
+        THEN("a.y == -4.2") {
+            REQUIRE(a.y_value == Approx(-4.2));
+        }
+        THEN("a.z == 3.1") {
+            REQUIRE(a.z_value == Approx(3.1));
+        }
+        THEN("a.w == 1.0") {
+            REQUIRE(a.w_value == Approx(1.0));
+        }
+        THEN("a is a point") {
+            REQUIRE(a.is_point());
+        }
+        THEN("a is not a vector") {
+            REQUIRE_FALSE(a.is_vector());
+        }
+    }
+}
+
+SCENARIO("A tuple with w = 0 is a vector", "[tuple][bdd]") {
+    GIVEN("a <- typle(4.3, -4.2, 3.1, 1.0)") {
+        Tuple a(4.3, -4.2, 3.1, 0.0);
+
+        THEN("a.x == 4.3") {
+            REQUIRE(a.x_value == Approx(4.3));
+        }
+        THEN("a.y == -4.2") {
+            REQUIRE(a.y_value == Approx(-4.2));
+        }
+        THEN("a.z == 3.1") {
+            REQUIRE(a.z_value == Approx(3.1));
+        }
+        THEN("a.w == 0.0") {
+            REQUIRE(a.w_value == Approx(0));
+        }
+        THEN("a is not a point") {
+            REQUIRE_FALSE(a.is_point());
+        }
+        THEN("a is not a vector") {
+            REQUIRE(a.is_vector());
+        }
+    }
+}
+
+SCENARIO("Magnitude and normalization", "[tuple][bdd]") {
+    GIVEN("a vector (1,2,3)") {
+        Tuple v = Tuple::vector(1, 2, 3);
+
+        WHEN("magnitude is computed") {
+            THEN("it equals sqrt(14)") {
+                REQUIRE(magnitude(v) == Approx(std::sqrt(14.0)));
+            }
+        }
+
+        WHEN("the vector is normalized") {
+            Tuple n = normalize(v);
+            THEN("the result is the normalized vector") {
+                REQUIRE(n == Tuple(1 / std::sqrt(14.0), 2 / std::sqrt(14.0), 3 / std::sqrt(14.0), 0.0));
+            }
+        }
+    }
+}
